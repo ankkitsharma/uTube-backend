15602d59a92d7e5c5c7fb049c3f494695f010592
ankkitsharma <ankitsharma2801@gmail.com>
2024-09-28 11:43:13 +0530

    fix incomingRefreshToken

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -180,7 +180,7 @@ export const refreshAccessToken = asyncHandler(async function (req, res, next) {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized request");
   }
 
